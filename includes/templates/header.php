1d0d6974bad7ea9e2139a11f7bca8258e26e1313
jimycox <terraxted@gmail.com>
2020-05-19 20:41:13 -0500

    actualización de menú mobile

--- includes/templates/header.php
+++ includes/templates/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Blog de viajes</title>
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+    <!--<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">-->
     <link rel="stylesheet" href="css/normalize.css">
     <link rel="stylesheet" href="css/video-js.css"> 
     <script type ="text/javascript" src="js/video.js"></script>
@@ -18,9 +18,24 @@
         <img src="img/logo.png" alt="logotipo">
     </header>
     <div class="barra">
-        <a href="#nav" class="mobile-menu">
-            <i class="fas fa-ellipsis-v"></i>
-        </a>
+        <label class="mobile-menu contenedor trigger" for="element-toggle" id="trigger-toggle"><i class="fas fa-bars"></i></label>
+        <input type="checkbox" id="element-toggle">
+
+ 
+        <nav class="navegacion-principal contenedor">
+            <a href="index.php">Inicio</a>
+            <a href="nosotros.php">Nosotros</a>
+            <a href="usuarios.php">Nuestros Usuarios</a>
+            <a href="tiendaVirtual.php">Tienda Virtual</a>
+            <a href="contacto.php">Contacto</a>
+            <!--<a href="#" class="cerrar">
+                <i class="fas fa-times"></i>
+            </a>-->
+        </nav>
+
+        <!--<label for="element-toggle" id="trigger-toggle"><i class="fas fa-ellipsis-v"></i></label>
+        <input type="checkbox" id="element-toggle">
+
         <nav id="nav" class="navegacion-principal contenedor">
             <a href="index.php">Inicio</a>
             <a href="nosotros.php">Nosotros</a>
@@ -30,5 +45,6 @@
             <a href="#" class="cerrar">
                 <i class="fas fa-times"></i>
         </a>
-        </nav>
+        </nav>-->
+        
     </div>
